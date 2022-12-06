cad955b0aab513d69fe72fcbba96097fe8e7d7cc
Chris Wadsworth <cjwadswo@uno.edu>
2022-12-06 13:44:36 -0600

    Made loadHtml a module to import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import myImage1 from "./image1.jpg";
 import myImage2 from "./image2.jpg";
 import myImage3 from "./image3.jpg";
+import { loadHtml } from "./tab-load";
 
 const image1 = document.getElementById("img1");
 const image2 = document.getElementById("img2");
@@ -12,71 +13,14 @@ const aboutTabBtn = document.getElementById("about");
 const contactTabBtn = document.getElementById("contact");
 
 homeTabBtn.addEventListener("click", changeTab);
-console.log(aboutTabBtn);
 aboutTabBtn.addEventListener("click", changeTab);
 contactTabBtn.addEventListener("click", changeTab);
+
+loadHtml("home");
 
 function changeTab(e) {
   let currentTab = document.querySelector('[data-selected="true"]');
   currentTab.dataset.selected = false;
   e.target.dataset.selected = true;
   loadHtml(e.target.id);
-}
-
-function loadHtml(tab) {
-  const content = document.getElementById("content");
-  content.innerHTML = "";
-  if (tab == "home") {
-    content.innerHTML = `<div class="box">
-    <h2>Welcome to the restraunt!</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quidem
-      soluta error tempore doloremque. Facilis molestias numquam mollitia
-      quasi omnis laborum quia rerum consequuntur tenetur voluptate
-      repellendus quisquam itaque ex, molestiae ducimus enim iure dolorem?
-      Harum minus illum quis dolorem delectus debitis fugit, magni placeat
-      omnis animi consequatur veniam aperiam, quod laborum. Nihil enim
-      fugiat officia adipisci quam sed magni unde laborum quod assumenda
-      expedita exercitationem dignissimos, minus deserunt nisi? Iusto,
-      repellat. Rerum praesentium porro at amet veniam neque vitae labore
-      enim vero, similique minima alias repellat repellendus facilis dolor
-      reiciendis, laborum iusto voluptate. Excepturi tenetur officiis id
-      ducimus dolor.
-    </p>
-  </div>`;
-  } else if (tab == "about") {
-    content.innerHTML = `<h1 class="founded">Founded in 1989</h1>
-    <div class="values-card">
-      <h2>Our values</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quae, illum
-        maiores saepe quasi itaque earum, facere hic a reiciendis nam sequi quaerat
-        excepturi. Optio, fugiat. Maiores architecto blanditiis consequuntur?
-      </p>
-    </div>
-    
-    <div class="values-card">
-      <h2>Our history</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi unde nihil
-        dolorem harum, officiis nesciunt reprehenderit aut dolore, blanditiis,
-        libero impedit doloribus optio quod. Nulla perspiciatis cupiditate quibusdam
-        voluptatem laboriosam?
-      </p>
-    </div>`;
-  } else if (tab == "contact") {
-    content.innerHTML = `<div class="contact-card">
-    <h1>Contact Us!</h1>
-    <li>
-      <ul>
-        Phone: (111)-111-1111
-      </ul>
-      <ul>
-        Address: 1111 Fake St
-        <br>Fake City, NY 10001</br>
-      </ul>
-      <ul>email: fakeemail@fake.com</ul>
-    </li>
-  </div>`;
-  }
 }
--- src/tab-load.js
+++ src/tab-load.js
@@ -1,11 +1,58 @@
 //Create button listeners for tabs
-const homeTabBtn = document.getElementById("home");
-const aboutTabBtn = document.getElementById("about");
-const contactTabBtn = document.getElementById("contact");
-
-homeTabBtn.addEventListener("click", changeTab);
-
-function changeTab(e) {
-  const currentTab = document.querySelector('[data-selected]="true"');
-  currentTab.dataset.selected = false;
+export function loadHtml(tab) {
+  const content = document.getElementById("content");
+  content.innerHTML = "";
+  if (tab == "home") {
+    content.innerHTML = `<div class="box">
+      <h2>Welcome to the restraunt!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quidem
+        soluta error tempore doloremque. Facilis molestias numquam mollitia
+        quasi omnis laborum quia rerum consequuntur tenetur voluptate
+        repellendus quisquam itaque ex, molestiae ducimus enim iure dolorem?
+        Harum minus illum quis dolorem delectus debitis fugit, magni placeat
+        omnis animi consequatur veniam aperiam, quod laborum. Nihil enim
+        fugiat officia adipisci quam sed magni unde laborum quod assumenda
+        expedita exercitationem dignissimos, minus deserunt nisi? Iusto,
+        repellat. Rerum praesentium porro at amet veniam neque vitae labore
+        enim vero, similique minima alias repellat repellendus facilis dolor
+        reiciendis, laborum iusto voluptate. Excepturi tenetur officiis id
+        ducimus dolor.
+      </p>
+    </div>`;
+  } else if (tab == "about") {
+    content.innerHTML = `<h1 class="founded">Founded in 1989</h1>
+      <div class="values-card">
+        <h2>Our values</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quae, illum
+          maiores saepe quasi itaque earum, facere hic a reiciendis nam sequi quaerat
+          excepturi. Optio, fugiat. Maiores architecto blanditiis consequuntur?
+        </p>
+      </div>
+      
+      <div class="values-card">
+        <h2>Our history</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi unde nihil
+          dolorem harum, officiis nesciunt reprehenderit aut dolore, blanditiis,
+          libero impedit doloribus optio quod. Nulla perspiciatis cupiditate quibusdam
+          voluptatem laboriosam?
+        </p>
+      </div>`;
+  } else if (tab == "contact") {
+    content.innerHTML = `<div class="contact-card">
+      <h1>Contact Us!</h1>
+      <li>
+        <ul>
+          Phone: (111)-111-1111
+        </ul>
+        <ul>
+          Address: 1111 Fake St
+          <br>Fake City, NY 10001</br>
+        </ul>
+        <ul>email: fakeemail@fake.com</ul>
+      </li>
+    </div>`;
+  }
 }
